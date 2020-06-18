7c5aad2667184bdf518399e32df45d037b4ab140
Luciano Pereira Gaspar <lpgaspar@gmail.com>
2020-06-18 00:08:14 -0300

    Adiiconei tracos

--- ExemploGIT/Program.cs
+++ ExemploGIT/Program.cs
@@ -3,9 +3,11 @@
 namespace ExemploGIT {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("-------------------------------------");
             Console.WriteLine("Hello World!");            
             Console.WriteLine("Como está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("-------------------------------------");
 
             Console.ReadLine();
         }
